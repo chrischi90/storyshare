ece344cf949719eaddab94febd1e8abb939625c4
Chris Chi <chrischi90@gmail.com>
2017-03-06 19:41:58 -0500

    using ckeditor rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,10 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
+//= require ckeditor-jquery
 //= require turbolinks
 //= require_tree .
-//= require ckeditor/init
+
+$('.ckeditor').ckeditor({
+  // optional config
+});
